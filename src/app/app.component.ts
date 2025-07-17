594c93449cf953c16ec3e7ced16b4024b42ee6d3
TDT <xuantruong0944@gmail.com>
2025-07-17 17:33:43 +0700

    add v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,77 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+// Interfaces
+interface Province {
+  ma_tinh: string;
+  ten_tinh: string;
+}
+
+
+interface ExamResult {
+  STT: string;
+  file_name: string;
+  modified_date: string;
+  Id: string;
+  TinhId: number;
+  DM1: number;
+  DM2: number;
+  DM3: number;
+  DM4: number;
+  DM5: number;
+  DM6: number;
+  DM7: number;
+  DM8: number;
+  DM9: number;
+  DM10: number;
+  DM11: number;
+  DM12: number;
+  DM13: number;
+  MA_MON_NGOAI_NGU: string;
+  SBD: string;
+  TONGDIEM: number;
+  TOAN: number;
+  VAN: number;
+  NGOAI_NGU: number;
+  SU: number;
+  DIA: number;
+  GDKT_PL: number;
+  LI: number;
+  HOA: number;
+  SINH: number;
+  TIN_HOC: number;
+  GIAO_DUC_CONG_DAN: number;
+  CN_CONG_NGHIEP: number;
+  CN_NONG_NGHIEP: number;
+  NGAY_SINH: string;
+  ten_tinh?: string;
+}
+
+interface SubjectInfo {
+  key: keyof ExamResult;
+  name: string;
+  color: string;
+}
+
+interface ScoreRange {
+  range: string;
+  count: number;
+  percentage: string;
+}
+
+interface ProvinceStats {
+  ma_tinh: string;
+  ten_tinh: string;
+  avgScore: number;
+  totalStudents: number;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'exam-score';
+  provinces: Province[] = [];
+
 }
